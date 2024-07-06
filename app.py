12dcfed5c8e13bc342ec83cdd035a7cd0dcb4d6d
Clifford Mapesa <cliffordmapesa8@gmail.com>
2024-07-06 18:50:01 +0300

    feat: add config

---
+++ app.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+"""Main Entry."""
+from os import getenv
+from flask import Flask, request, jsonify
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+from flask_jwt_extended import JWTManager, create_access_token, jwt_required, get_jwt_identity
+from werkzeug.security import generate_password_hash, check_password_hash
+import uuid
+
+app = Flask(__name__)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://username:password@localhost/db_name'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['JWT_SECRET_KEY'] = 'your_jwt_secret_key'
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+jwt = JWTManager(app)
